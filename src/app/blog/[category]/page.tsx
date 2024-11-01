534bbfa2dc0f84ad425a5ab9c533036eff546adc
Alexandru Kelemen <kelemen.laszlo994@gmail.com>
2024-11-01 11:20:37 +0100

    third commit

---
+++ src/app/blog/[category]/page.tsx
@@ -0,0 +1,52 @@
+import CardCategory from "@/components/CardCategory";
+import { getBlogPosts } from "../utils";
+import { Link } from "lucide-react";
+import Container from "@/components/Container";
+import Header from "@/components/Header";
+
+export default function Page({ params }: { params: { category: string } }) {
+  const posts = getBlogPosts().filter(
+    (post) => post.metadata.category === params.category
+  );
+
+  if (!posts) {
+    notFound();
+  }
+  return (
+    <>
+      <Header>
+        <Container>
+          <h1 className="title font-semibold text-2xl tracking-wider mt-4 uppercase">
+            {posts[0]?.metadata.category}
+          </h1>
+        </Container>
+      </Header>
+      <Container>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
+          {posts
+            .sort((a, b) => {
+              if (
+                new Date(a.metadata.publishedAt) >
+                new Date(b.metadata.publishedAt)
+              ) {
+                return -1;
+              }
+              return 1;
+            })
+            .map((post) => (
+              <Link
+                href={`/blog/${post.metadata.category}/${post.slug}`}
+                key={post.slug}
+              >
+                <CardCategory
+                  title={post.metadata.title}
+                  summary={post.metadata.summary}
+                  date={post.metadata.publishedAt}
+                />
+              </Link>
+            ))}
+        </div>
+      </Container>
+    </>
+  );
+}
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,9 @@
-"use client";
-
 import { POSTS } from "@/lib/constants";
 import { Icons } from "./icons";
 import Link from "next/link";
-// import { Input } from "./ui/input";
-import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-// import { createSubscriber } from "@/lib/actions";
-// import { useFormState } from "react-dom";
-
+import { Button } from "./ui/button";
 export default function Footer() {
-  //   const initialState = { message: "", errors: {} };
-  //   const [state, dispatch] = useFormState(createSubscriber, initialState);
   return (
     <footer className="bg-gray-100 py-8 dark:bg-gray-800 mt-10">
       <div className="container mx-auto px-4 md:px-6">
@@ -19,22 +11,22 @@ export default function Footer() {
           <div className="space-y-4">
             <div className="flex items-center space-x-2">
               <Icons.logo className="h-6 w-6" />
-              <span className="text-md font-semibold">Cyberscribe</span>
+              <span className="text-md font-semibold">Coding Jitsu</span>
             </div>
             <p className="text-gray-500 dark:text-gray-400 text-sm">
               Stay Up to Date with the latest news and insights from our blog.
             </p>
             <div className="flex space-x-4">
-              {/* <a
+              <a
                 href="https://twitter.com/w3tsadev"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Twitter"
-              > */}
-              {/* <Icons.twitter className="h-6 w-6 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300" />
-              </a> */}
+              >
+                <Icons.twitter className="h-6 w-6 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300" />
+              </a>
               <a
-                href="https://github.com/lallaPNG"
+                href="https://github.com/w3tsadev"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Github"
@@ -85,14 +77,6 @@ export default function Footer() {
                   Privacy Policy
                 </Link>
               </li>
-              <li>
-                <Link
-                  href="/sitemap.xml"
-                  className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
-                >
-                  Sitemap
-                </Link>
-              </li>
             </ul>
           </div>
           <div className="space-y-4">
@@ -101,37 +85,22 @@ export default function Footer() {
               Subscribe to our newsletter to stay up-to-date with the latest
               news and updates.
             </p>
-            {/* <form action={dispatch}>*/}
             <form>
               <div className="flex space-x-2">
-                <Input type="email" />
+                <Input
+                  type="email"
+                  placeholder="Enter your email"
+                  className="flex-1"
+                />
                 <Button>Subscribe</Button>
               </div>
-              {/* <div
-              id="email-error"
-              aria-label="polite"
-              aria-atomic="true"
-              className="px-1"
-            > */}
-              {/* {state?.errors?.email &&
-                  state.errors.email.map((error: string) => (
-                    <p key={error} className="text-xs text-red-500">
-                      {error}
-                    </p>
-                  ))}
-                {!state?.errors?.email && (
-                  <p className="text-xs text-green-500">{state?.message}</p>
-                )} */}
-              {/* </div>
-            </form>  */}
             </form>
           </div>
         </div>
+        <div className="mt-8 border-t border-gray-200 pt-4 text-center text-xs text-gray-500 dark:border-gray-700 dark:text-gray-400">
+          &copy; 2024 Coding Jitsu. All rights reserved.
+        </div>
       </div>
-      <div className="mt-8 border-t border-gray-200 pt-4 text-center text-xs text-gray-500 dark:border-gray-700 dark:text-gray-400">
-        &copy; 2024 Cyberscribe. All rights reserved.
-      </div>
-      {/* </div> */}
     </footer>
   );
 }
